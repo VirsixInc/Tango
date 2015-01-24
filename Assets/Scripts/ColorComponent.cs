d2ce3fbd925c2c5473bbd8fa3e864d8687642b6c
Kenneth Chong <kenneth.s.chong@gmail.com>
2015-01-24 13:24:54 -0800

    Added ColorComponent, which can be attached to GameObjects to assign them a specific color, such as red or blue.

---
+++ Assets/Scripts/ColorComponent.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorComponent : MonoBehaviour 
+{
+	public enum pColor
+	{
+		grey = 0,
+		blue = 1,
+		red = 2
+	}
+
+	public pColor currentColor;
+	private bool colorChanged;
+
+	// Use this for initialization
+	void Start () 
+	{
+		SetMaterialColor();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	void ForceMissingColor()
+	{
+
+	}
+	
+	void SetMaterialColor()
+	{
+		if (currentColor == pColor.blue)
+			renderer.material.color = Color.blue;
+		else if (currentColor == pColor.red)
+			renderer.material.color = Color.red;
+		else
+			renderer.material.color = Color.white;
+	}
+
+	bool IsSameColor(pColor color)
+	{
+		return currentColor == color;
+	}
+}
