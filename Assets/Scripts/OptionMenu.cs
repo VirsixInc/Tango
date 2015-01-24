c18f71697a1319b77d8fa748cbea92fb71ca7597
Zack <zacharyrock@hotmail.com>
2015-01-24 02:28:17 -0800

    GameManager and PauseMenu done.

---
+++ Assets/Scripts/OptionMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OptionMenu : MonoBehaviour {
+	
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+	public void UpdateEffectsVolume(float volume) {
+		GameManager.effectsVolume = (int)volume;
+		transform.FindChild("Volume").FindChild ("Sound Effects").GetComponent<Text> ().text = "Sound Effects: " + (int)volume;
+	}
+
+	public void UpdateMusicVolume(float volume) {
+		GameManager.musicVolume = (int)volume;
+		transform.FindChild("Volume").FindChild ("Music").GetComponent<Text> ().text = "Music: " + (int)volume;
+	}
+}
