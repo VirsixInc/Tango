8450907006c8b74b5abe5639160e0e6a0c4794ba
Eric Espinoza <eric@virsix.com>
2015-01-24 23:47:55 -0800

    Audio: - Changing volume during game works - Main menu music plays and game music play when they are supposed to

--- Assets/Scripts/OptionMenu.cs
+++ Assets/Scripts/OptionMenu.cs
@@ -3,7 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class OptionMenu : MonoBehaviour {
-	
+
+	public delegate void EffectVolumeAction();
+	public static event EffectVolumeAction ModifyEffectVolume;
+
+	public delegate void MusicVolumeAction();
+	public static event MusicVolumeAction ModifyMusicVolume;
+
+
 	void Start () {
 	
 	}
@@ -15,10 +22,18 @@ public class OptionMenu : MonoBehaviour {
 	public void UpdateEffectsVolume(float volume) {
 		GameManager.effectsVolume = (int)volume;
 		transform.FindChild("Volume").FindChild ("Sound Effects").GetComponent<Text> ().text = "Sound Effects: " + (int)volume;
+
+		if( ModifyEffectVolume != null ) {
+			ModifyEffectVolume();
+		}
 	}
 
 	public void UpdateMusicVolume(float volume) {
 		GameManager.musicVolume = (int)volume;
 		transform.FindChild("Volume").FindChild ("Music").GetComponent<Text> ().text = "Music: " + (int)volume;
+
+		if( ModifyMusicVolume != null ) {
+			ModifyMusicVolume();
+		}
 	}
 }
