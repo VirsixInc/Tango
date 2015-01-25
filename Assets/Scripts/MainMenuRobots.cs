5bd8586d49c0291fbbf8b2b9da4a2b83b02b5655
Ben Zerr <bzhurley@gmail.com>
2015-01-25 14:25:28 -0800

    Made prefab and script for main menu "animations"

---
+++ Assets/Scripts/MainMenuRobots.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuRobots : MonoBehaviour 
+{
+	float timer;
+	int ang;
+	float randTime;
+	// Use this for initialization
+	void Start () 
+	{
+		timer = 0.0f;
+		ang = Random.Range (-5, 5) * 25;
+		randTime = Random.Range (0.75f, 2.0f);
+	}
+
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if(timer > randTime)
+		{
+			timer = 0.0f;
+			ang = Random.Range(-5, 5) * 25;
+			randTime = Random.Range (0.75f, 2.0f);
+		}
+		transform.RotateAround(transform.position, Vector3.forward, ang * Time.deltaTime);
+	}
+
+}
