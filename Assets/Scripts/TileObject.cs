85bbee7cd97504927128974ae1058782e728cd03
Zack <zacharyrock@hotmail.com>
2015-01-24 19:15:48 -0800

    SteppedOff tileobject. Teleporter added

--- Assets/Scripts/TileObject.cs
+++ Assets/Scripts/TileObject.cs
@@ -6,10 +6,13 @@ public class TileObject : MonoBehaviour
 	public enum Type
 	{
 		BUTTON,
+		TELEPORTER,
 		NONE
 	}
 
 	public Type type;
+
+
 	
 	public void SteppedOn(ColorComponent.pColor color)
 	{
@@ -19,6 +22,31 @@ public class TileObject : MonoBehaviour
 			ColorComponent.pColor buttonColor = gameObject.GetComponent<GameButton>().GetColor();
 			if( buttonColor == color || buttonColor == ColorComponent.pColor.grey )
 				gameObject.GetComponent<GameButton>().StepOn();
+			break;
+		case Type.TELEPORTER:
+			ColorComponent.pColor teleporterColor = gameObject.GetComponent<Teleporter>().GetColor();
+			if(teleporterColor == color)
+				gameObject.GetComponent<Teleporter>().StepOn();
+			break;
+		case Type.NONE:
+			break;
+		default:
+			break;
+		}
+	}
+
+	public void SteppedOff(ColorComponent.pColor color) {
+		switch(type)
+		{
+		case Type.BUTTON:
+			ColorComponent.pColor buttonColor = gameObject.GetComponent<GameButton>().GetColor();
+			if( buttonColor == color || buttonColor == ColorComponent.pColor.grey )
+				gameObject.GetComponent<GameButton>().StepOff();
+			break;
+		case Type.TELEPORTER:
+			ColorComponent.pColor teleporterColor = gameObject.GetComponent<Teleporter>().GetColor();
+			if(teleporterColor == color)
+				gameObject.GetComponent<Teleporter>().StepOff();
 			break;
 		case Type.NONE:
 			break;
